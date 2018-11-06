1295dc9db66ddbefe26d4c75f9e624d020089f8e
andreighervan <andrei.ghervan8@gmail.com>
2018-11-06 13:16:25 +0200

    Add models

---
+++ models/Expense.js
@@ -0,0 +1,9 @@
+//models/Expense.js
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var expenseSchema = new Schema({
+    description: String,
+    amount: Number,
+    month: String,
+    year: Number
+});
--- server/server.js
+++ server/server.js
@@ -5,6 +5,9 @@ var path = require('path');
 var app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../client'));
+
+DB='mongodb://andreighervan:123QQsuccess@ds153593.mlab.com:53593/expense-manager'
+
 app.use(express.static(path.join(__dirname, '../client')));
 app.use('/', router);
 
