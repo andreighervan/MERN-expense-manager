d0b7ff134c6c39d307de8f71ebe4899f33020458
andreighervan <andrei.ghervan8@gmail.com>
2018-12-14 12:18:43 +0200

    Add month tabs component to app.js

--- client/components/App.js
+++ client/components/App.js
@@ -7,6 +7,7 @@ import Update from './Update';
 import Delete from './Delete';
 import { Tab, Tabs } from 'react-bootstrap';
 import YearTabsRouter from './tabs/yearTabsRouter';
+import MonthTabs from './tabs/monthTabs';
 export default class App extends React.Component {
     constructor() {
         super();
@@ -47,11 +48,11 @@ export default class App extends React.Component {
         return (
             <div>
                 <Tabs activeKey={this.state.activeTab} onSelect={this.handleSelect}>
-                    <Tab eventKey={2016} title={<YearTabsRouter year='2016' />}></Tab>
-                    <Tab eventKey={2017} title={<YearTabsRouter year='2017' />}></Tab>
-                    <Tab eventKey={2018} title={<YearTabsRouter year='2018'/>}></Tab>
-                    <Tab eventKey={2019} title={<YearTabsRouter year='2019'/>}></Tab>
-                    <Tab eventKey={2020} title={<YearTabsRouter year='2020'/>}></Tab>
+                    <Tab eventKey={2016} title={<YearTabsRouter year='2016' />}><MonthTabs year='2016' monthlyActiveTab={this.state.selectedMonth}/></Tab>
+                    <Tab eventKey={2017} title={<YearTabsRouter year='2017' />}><MonthTabs year='2017' monthlyActiveTab={this.state.selectedMonth}/></Tab>
+                    <Tab eventKey={2018} title={<YearTabsRouter year='2018'/>}><MonthTabs year='2018' monthlyActiveTab={this.state.selectedMonth}/></Tab>
+                    <Tab eventKey={2019} title={<YearTabsRouter year='2019'/>}><MonthTabs year='2019' monthlyActiveTab={this.state.selectedMonth}/></Tab>
+                    <Tab eventKey={2020} title={<YearTabsRouter year='2020'/>}><MonthTabs year='2020' monthlyActiveTab={this.state.selectedMonth}/></Tab>
                 </Tabs>
                 <Add selectedMonth={this.state.selectedMonth} selectedYear={this.state.selectedYear} />
                 <table>
